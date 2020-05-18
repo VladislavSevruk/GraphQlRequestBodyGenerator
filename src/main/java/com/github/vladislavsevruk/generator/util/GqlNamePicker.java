840dab58930f28e3c726eb430caf14576abf3421
Uladzislau Seuruk <vladislavsevruk@gmail.com>
2020-05-18 19:55:38 +0300

    Implemented support for alias and field arguments

--- src/main/java/com/github/vladislavsevruk/generator/util/GqlNamePicker.java
+++ src/main/java/com/github/vladislavsevruk/generator/util/GqlNamePicker.java
@@ -24,8 +24,11 @@
 package com.github.vladislavsevruk.generator.util;
 
 import com.github.vladislavsevruk.generator.annotation.GqlField;
+import com.github.vladislavsevruk.generator.annotation.GqlFieldArgument;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * Picks name for GraphQL items.
@@ -47,5 +50,41 @@ public final class GqlNamePicker {
             return fieldAnnotation.name();
         }
         return field.getName();
+    }
+
+    /**
+     * Gets name for GraphQL operation from {@link GqlField} annotation with field alias and arguments if present or
+     * using field name.
+     *
+     * @param field <code>Field</code> to get name, alias and arguments for.
+     * @return <code>String</code> with field name, alias and arguments.
+     */
+    public static String getFieldNameWithArgumentsAndAlias(Field field) {
+        String fieldName = getFieldName(field);
+        GqlField fieldAnnotation = field.getAnnotation(GqlField.class);
+        if (fieldAnnotation != null) {
+            fieldName = addAliasIfPresent(fieldName, fieldAnnotation);
+            fieldName = addArgumentsIfPresent(fieldName, fieldAnnotation);
+        }
+        return fieldName;
+    }
+
+    private static String addAliasIfPresent(String fieldName, GqlField fieldAnnotation) {
+        String alias = fieldAnnotation.alias();
+        return alias.isEmpty() ? fieldName : alias + ":" + fieldName;
+    }
+
+    private static String addArgumentsIfPresent(String fieldName, GqlField fieldAnnotation) {
+        GqlFieldArgument[] arguments = fieldAnnotation.arguments();
+        if (arguments.length != 0) {
+            String argumentValues = Arrays.stream(arguments).map(GqlNamePicker::generateArgumentValue)
+                    .collect(Collectors.joining(","));
+            fieldName += "(" + argumentValues + ")";
+        }
+        return fieldName;
+    }
+
+    private static String generateArgumentValue(GqlFieldArgument argument) {
+        return argument.name() + ":" + argument.value();
     }
 }
