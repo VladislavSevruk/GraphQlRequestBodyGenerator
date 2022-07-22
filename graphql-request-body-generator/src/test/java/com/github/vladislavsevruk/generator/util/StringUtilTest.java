522e8f4fcf56534ee459d0c174ea49e6103c68c7
VladislavSevruk <vladislavsevruk@gmail.com>
2022-07-22 17:24:22 +0200

    Fixed sonar smell

--- graphql-request-body-generator/src/test/java/com/github/vladislavsevruk/generator/util/StringUtilTest.java
+++ graphql-request-body-generator/src/test/java/com/github/vladislavsevruk/generator/util/StringUtilTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-public class StringUtilTest {
+class StringUtilTest {
 
     @Test
     void isBlankNullTest() {
