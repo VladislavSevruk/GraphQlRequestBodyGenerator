ea7839efb65504b025370f30fe062a700da50ec6
VladislavSevruk <vladislavsevruk@gmail.com>
2022-07-22 17:52:56 +0200

    Updated copyrights

--- graphql-request-body-generator/src/main/java/com/github/vladislavsevruk/generator/generator/mutation/GqlMutationBodyGenerator.java
+++ graphql-request-body-generator/src/main/java/com/github/vladislavsevruk/generator/generator/mutation/GqlMutationBodyGenerator.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020-2021 Uladzislau Seuruk
+ * Copyright (c) 2020-2022 Uladzislau Seuruk
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
